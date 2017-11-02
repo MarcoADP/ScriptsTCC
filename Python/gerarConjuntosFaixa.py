ef6f64a907f839c37be17cfe6e855f9a4cf23c5c
Marco Aurélio Paulino <marco23_aurelio@hotmail.com>
2017-11-02 11:14:06 -0200

    acertos na geracao dos conjuntos de faixa

--- Python/gerarConjuntosFaixa.py
+++ Python/gerarConjuntosFaixa.py
@@ -12,9 +12,12 @@ import os, os.path
 db = '70'
 LBPNeighbors = 8
 LBPRadius = 2
-classes = 4
+
+n_classes = 4
+n_audios = 5
+n_pessoas = 20
 n_conj = 3
-audio_classe = 50;
+audio_classe = 100;
 
 LBPName = 'lbp' + str(LBPNeighbors) + str(LBPRadius)
 
@@ -26,30 +29,60 @@ if not os.path.exists(DIR_OUT):
     os.makedirs(DIR_OUT)
 
 size_files = len([name for name in os.listdir(DIR) if os.path.isfile(os.path.join(DIR, name))])
-print(os.listdir(DIR))
-print (size_files)
 
 conjuntos = ["" for x in range(n_conj+1)]
-c_atual = [0 for x in range(classes+1)]
+classes = ["" for x in range(n_conj+1)]
 
-for i in range(1, size_files):
+filename_out = ["" for x in range(n_conj+1)]
+file_out = ["" for x in range(n_conj+1)]
 
-    classe = ((i-1) // 100)+1
 
-    conjunto = (c_atual[classe] % 3) + 1
-    c_atual[classe] = c_atual[classe] + 1
 
-    print(str(i) + " -> " + str(classe) + " -- " + str(conjunto));
-    filename = DIR + str(i) + ".txt";
-    text = open(filename, 'r');
-    valores = text.read();
-    conjuntos[conjunto] = conjuntos[conjunto] + str(classe) + " " + str(valores);
-    if(i < size_files-2):
-        conjuntos[conjunto] = conjuntos[conjunto] + '\n'
+
+for c in range(0, n_classes):
+    cj = (c+1) % 3;
+    if(cj == 0):
+        cj = 3
+
+    #if(c == 0 or c == 1):
+    #    caux = 0
+    #elif(c == 2 or c == 3):
+    #    caux = 1
+    #elif(c == 4 or c == 5):
+    #    caux = 2
+    #elif(c == 6 or c == 7):
+    #    caux = 3
+
+    for m in range(1, n_audios+1):
+        for p in range(0, n_pessoas):
+            pos = c * audio_classe + p * n_audios + m
+            filename = DIR + str(pos) + ".txt";
+            text = open(filename, 'r');
+            valores = text.read();
+
+            if(conjuntos[cj] != ""):
+                conjuntos[cj] = conjuntos[cj] + '\n'
+                classes[cj] = classes[cj] + '\n'
+
+            print(str(pos) + " -> " + str(c) + " -- " + str(cj));
+            conjuntos[cj] = conjuntos[cj] + str(valores)
+            classes[cj] = classes[cj] + str(c)
+
+
+            cj = cj + 1;
+            if(cj > 3):
+                cj = 1
 
 
 for i in range(1, n_conj+1):
+    f_classe_out = "resposta_" + str(i) + ".txt";
     filename_out = str(i) + ".txt";
+
     file_out = open(DIR_OUT + filename_out, 'w')
+    file_classe_out = open(DIR_OUT + f_classe_out, 'w')
+
     file_out.write(conjuntos[i]);
+    file_classe_out.write(classes[i])
+
     file_out.close();
+    file_classe_out.close();
